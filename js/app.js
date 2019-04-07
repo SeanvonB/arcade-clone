4466ff335c259edc24e223844cf60537dd23fcb8
Sean von Bayern <seanvonbayern@gmail.com>
2019-04-07 16:20:00 -0700

    Create and instantiate actor classes

--- js/app.js
+++ js/app.js
@@ -1,39 +1,69 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// Enemies that the player must avoid
+class Enemy {
+    constructor() {
+        // Location on screen
+        this.x = 0;
+        this.y = 141; // 166 minus 25 centers the sprite visually
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        // Assign image to enemy sprite
+        this.sprite = 'images/enemy-bug.png';
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Draw enemy sprite on the screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Update enemy position as location values change
+    update(dt) {
+        // Update position based on movement*dt
+    }
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Hero who the player controls
+class Hero {
+    constructor(heroType) {
+        // Location on screen
+        this.x = 202;
+        this.y = 375; // 415 minus 40 centers the sprite visually
+
+        // Game stats
+        this.health = 1;
+
+        // Assign image to hero sprite
+        this.sprite = "images/char-boy.png";
+    }
+
+    // Change sprite location based on player inputs
+    handleInput(inputKey) {
+        if (inputKey === "left" && this.x > 0) {this.x -= 101;}
+        else if (inputKey === "up" && this.y > 0) {this.y -= 83;}
+        else if (inputKey === "right" && this.x < 404) {this.x += 101;}
+        else if (inputKey === "down" && this.y < 375) {this.y += 83;}
+    }
+
+    // Draw player sprite on screen
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Update player position as location values change
+    update(dt) {
+        // Update position based on movement*dt
+    }
+}
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Instantiate actors
+const allEnemies = [];
+const enemy1 = new Enemy();
+allEnemies.push(enemy1);
+
+const player = new Hero();
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Listen for key presses and sends them to .handleInput()
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
